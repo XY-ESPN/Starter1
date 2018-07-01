0e7cd078f971eb9ae0b57668ae08866382a6af37
XY-ESPN <40702469+XY-ESPN@users.noreply.github.com>
2018-07-01 12:20:55 +0530

    Version 1
    Downloaded from Spring Initializr without any dependencies

---
+++ sports_plus/src/main/java/com/it/sports_plus/SportsPlusApplication.java
@@ -0,0 +1,12 @@
+package com.it.sports_plus;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SportsPlusApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SportsPlusApplication.class, args);
+	}
+}
